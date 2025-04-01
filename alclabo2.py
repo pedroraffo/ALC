b44a88adb0634e547140e52ecc9dec6f60c27962
pedroraffo <165916629+pedroraffo@users.noreply.github.com>
2025-04-01 12:56:07 -0300

    Add files via upload

---
+++ alclabo2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  1 12:44:45 2025
+
+@author: Estudiante
+"""
+
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt 
+
+#1
+
+T = np.array([[2, 0], [0, 2]])
+t = np.array([[0.5, 0], [0, 0.5]])
+
+print (T @ t)
+
+ 
